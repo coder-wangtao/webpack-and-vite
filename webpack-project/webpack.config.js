ff79ac45d7dc7ffbcc1e420eb92f8310759d50c6
w t <1475929340@qq.com>
2025-08-11 13:27:59 +0800

    .

--- webpack-project/webpack.config.js
+++ webpack-project/webpack.config.js
@@ -1,11 +1,28 @@
 const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js", // 入口文件
+  entry: "./src/index.jsx", // 入口文件
   output: {
     filename: "bundle.js", // 输出文件名
     path: path.resolve(__dirname, "dist"), // 输出目录
   },
   mode: "development", // 可以设置为 'production' 以进行优化和压缩
   devtool: "source-map", //查看打包后的代码更方便
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html", //将打包后的代码插入到html模版中
+    }),
+  ],
 };
